0e268b574707de429e2dfc494d94cc837c9e10ea
emerckx <ewoutmerckx.pro@gmail.com>
2017-01-23 20:54:41 +0100

    added basic hello world lua experiment

---
+++ lua_controllers/hello_world_controller.lua
@@ -0,0 +1,23 @@
+-----------------------------------------------------
+-- @author Ewout Merckx - <ewoutmerckx.pro@gmail.com>
+-----------------------------------------------------
+
+-- global variables
+
+
+-- initialization 
+function init()
+end
+
+-- actions for each step
+function step()
+    -- each step, log "Hello world."
+    log("Hello world.")
+end
+
+-- reset the controller
+function reset()
+end
+
+function destroy()
+end
